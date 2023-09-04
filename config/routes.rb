3bc2ede1952634ecebc44f52d3ab12dd1b4fdae8
SeiyaYamaguchi <141209682+seiya0331@users.noreply.github.com>
2023-09-04 10:31:29 +0000

    [Add]フォロー・フォロワー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
@@ -6,7 +8,14 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    member do
+      get :follows, :followers
+    end
+      resource :relationships, only: [:create, :destroy]
+  end
+    
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "home/about"=>"homes#about"
 end
