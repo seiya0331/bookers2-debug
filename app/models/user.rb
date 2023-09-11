3f0db415beb5f57a2385f5e0c59b352d5d0ee98d
SeiyaYamaguchi <141209682+seiya0331@users.noreply.github.com>
2023-09-11 05:44:57 +0000

    [Add]いいねの多い順に表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorired_books, through: :books, source: :book
   has_many :book_comments, dependent: :destroy
   has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
